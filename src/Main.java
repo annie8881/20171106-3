f0c7d1a361d667a910e9f928d98ad19788fbecdc
annie8881 <880801blue@gmail.com>
2017-11-06 14:18:20 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        if(n==3)
+            System.out.println("Spring");
+        else if(n==4)
+            System.out.println("Spring");
+        else if(n==5)
+            System.out.println("Spring");
+        else if(n==6)
+            System.out.println("Summer");
+        else if(n==7)
+            System.out.println("Summer");
+        else if(n==8)
+            System.out.println("Summer");
+        else if(n==9)
+            System.out.println("Autumn");
+        else if(n==10)
+            System.out.println("Autumn");
+        else if(n==11)
+            System.out.println("Autumn");
+        else if(n==12)
+            System.out.println("Winter");
+        else if(n==1)
+            System.out.println("Winter");
+        else if(n==2)
+            System.out.println("Winter");
+    }
+}
